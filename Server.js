fac2e270ce35ae17e394bfda2255ad5277d39266
Sebastian <sebastiancarrera2021@gmail.com>
2024-12-14 15:53:51 -0300

    Fix error consulta

--- Server.js
+++ Server.js
@@ -370,15 +370,15 @@ app.get('/conteo-asistencia/:usuarioId', (req, res) => {
 
   const queryClases = `
     SELECT 
-      c.id AS clase_id, 
+      c.id_clase AS clase_id, 
       c.nombre AS clase_nombre, 
       c.id_materia,
       COUNT(a.id_tp_asistencia) AS total_asistencias,
       SUM(CASE WHEN a.id_tp_asistencia = 1 THEN 1 ELSE 0 END) AS asistencias
     FROM clases c
-    LEFT JOIN asistencia a ON c.id = a.id_clase AND a.id_usuario = ?
+    LEFT JOIN asistencia a ON c.id_clase = a.id_clase AND a.id_usuario = ?
     WHERE c.id_materia IN (SELECT materia_id FROM usuario_materia WHERE usuario_id = ?)
-    GROUP BY c.id, c.id_materia;
+    GROUP BY c.id_clase, c.id_materia;
   `;
 
   // Ejecutar las dos consultas
@@ -394,6 +394,7 @@ app.get('/conteo-asistencia/:usuarioId', (req, res) => {
         return res.status(500).json({ message: 'Error al obtener las clases.' });
       }
 
+      // Transformar resultados para unir materias y clases
       const resultado = materias.map((materia) => {
         const clasesMateria = clases.filter((clase) => clase.id_materia === materia.materia_id);
 
